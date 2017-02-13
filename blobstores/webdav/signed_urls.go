86a861c9fbcd30f11ce796a6b1963e0488f80f5c
Peter Goetz <peter.gtz@gmail.com>
2017-02-14 00:03:05 +0100

    Make droplet upload work

--- blobstores/webdav/signed_urls.go
+++ blobstores/webdav/signed_urls.go
@@ -1,16 +1,27 @@
 package webdav
 
 import (
+	"crypto/tls"
 	"net/http"
+	"strings"
+
+	"fmt"
+	"time"
+
+	"io/ioutil"
 
 	"github.com/petergtz/bitsgo/config"
+	"github.com/petergtz/bitsgo/httputil"
 )
 
 func NewWebdavResourceSigner(config config.WebdavBlobstoreConfig) *WebdavResourceSigner {
 	return &WebdavResourceSigner{
 		webdavEndpoint: config.Endpoint,
 		// TODO: add correct timeouts etc.
-		httpClient: &http.Client{},
+		httpClient: &http.Client{
+			// TODO skip SSL validation
+			Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
+		},
 	}
 }
 
@@ -19,6 +30,35 @@ type WebdavResourceSigner struct {
 	webdavEndpoint string
 }
 
-func (signer *WebdavResourceSigner) Sign(resource string, method string) (signedURL string) {
-	return "TODO: signedURL"
+func (signer *WebdavResourceSigner) Sign(resource string, method string) string {
+	var url string
+	switch strings.ToLower(method) {
+	case "put":
+		// TODO why do we need a "/" before the resource?
+		url = fmt.Sprintf(signer.webdavEndpoint+"/sign_for_put?path=/%v&expires=%v", resource, time.Now().Unix()+3600)
+	case "get":
+		url = fmt.Sprintf(signer.webdavEndpoint+"/sign?path=/%v&expires=%v", resource, time.Now().Unix()+3600)
+	}
+	request, _ := http.NewRequest("GET", url, nil)
+	request.SetBasicAuth("blobstore", "blobstore")
+	response, e := signer.httpClient.Do(request)
+	if e != nil {
+		return "Error during signing. Error: " + e.Error()
+	}
+	if response.StatusCode != http.StatusOK {
+		return "Error during signing. Error code: " + response.Status
+	}
+	defer response.Body.Close()
+	content, e := ioutil.ReadAll(response.Body)
+	if e != nil {
+		return "Error reading response body. Error: " + e.Error()
+	}
+
+	signedUrl := httputil.MustParse(string(content))
+
+	// TODO Is this really what we want to do?
+	signedUrl.Host = httputil.MustParse(signer.webdavEndpoint).Host
+	signedUrl.Scheme = httputil.MustParse(signer.webdavEndpoint).Scheme
+
+	return signedUrl.String()
 }
--- blobstores/webdav/webdav_blobstore.go
+++ blobstores/webdav/webdav_blobstore.go
@@ -1,10 +1,12 @@
 package webdav
 
 import (
+	"crypto/tls"
 	"io"
 	"net/http"
-	"path/filepath"
 
+	"github.com/petergtz/bitsgo/config"
+	"github.com/petergtz/bitsgo/httputil"
 	"github.com/petergtz/bitsgo/logger"
 	"github.com/petergtz/bitsgo/routes"
 	"github.com/pkg/errors"
@@ -14,18 +16,22 @@ import (
 type Blobstore struct {
 	httpClient     *http.Client
 	webdavEndpoint string
+	signer         *WebdavResourceSigner
 }
 
 func NewBlobstore(webdavEndpoint string) *Blobstore {
 	return &Blobstore{
 		webdavEndpoint: webdavEndpoint,
-		// TODO: add correct timeouts etc.
-		httpClient: &http.Client{},
+		// TODO: add correct timeouts etc. Don't skip certifcate verification!
+		httpClient: &http.Client{
+			Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
+		},
+		signer: NewWebdavResourceSigner(config.WebdavBlobstoreConfig{Endpoint: webdavEndpoint}),
 	}
 }
 
 func (blobstore *Blobstore) Exists(path string) (bool, error) {
-	url := filepath.Join(blobstore.webdavEndpoint, path)
+	url := blobstore.webdavEndpoint + "/" + path
 	logger.Log.Debug("Exists", zap.String("path", path), zap.String("url", url))
 	response, e := blobstore.httpClient.Head(url)
 	if e != nil {
@@ -45,17 +51,18 @@ func (blobstore *Blobstore) Get(path string) (body io.ReadCloser, redirectLocati
 	if !exists {
 		return nil, "", routes.NewNotFoundError()
 	}
-	signedUrl, e := blobstore.requestSignedWebdavUrl(path)
+	signedUrl := blobstore.signer.Sign(path, "get")
+	// signedUrl, e := blobstore.requestSignedWebdavUrl(path)
 	if e != nil {
 		return nil, "", e
 	}
 	return nil, signedUrl, nil
 }
 
-func (blobstore *Blobstore) requestSignedWebdavUrl(path string) (string, error) {
-	blobstore.httpClient.Get(filepath.Join(blobstore.webdavEndpoint, "sign"))
-	return "", nil
-}
+// func (blobstore *Blobstore) requestSignedWebdavUrl(path string) (string, error) {
+// 	blobstore.httpClient.Get(filepath.Join(blobstore.webdavEndpoint, "sign"))
+// 	return "", nil
+// }
 
 func (blobstore *Blobstore) Head(path string) (redirectLocation string, err error) {
 	_, redirectLocation, e := blobstore.Get(path)
@@ -63,6 +70,22 @@ func (blobstore *Blobstore) Head(path string) (redirectLocation string, err erro
 }
 
 func (blobstore *Blobstore) Put(path string, src io.ReadSeeker) (redirectLocation string, err error) {
+	request, e := httputil.NewPutRequest(blobstore.webdavEndpoint+"/admin/"+path, map[string]map[string]io.Reader{
+		"dummy": map[string]io.Reader{"dummyfilename": src},
+	})
+	if e != nil {
+		return "", e
+	}
+	request.SetBasicAuth("blobstore", "blobstore")
+	response, e := blobstore.httpClient.Do(request)
+	if e != nil {
+		return "", errors.Wrapf(e, "TODO")
+	}
+	// TODO improve error handling. It should provide a better error to the caller, so that the caller can provide
+	//      a better response to its caller
+	if response.StatusCode != http.StatusCreated {
+		return "", errors.Errorf("Expected StatusCreated, but got status code: " + response.Status)
+	}
 	return "", nil
 }
 
